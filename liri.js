bd22b953f5120e62075c4f04905f03f93386e39e
rob bee <rbenny@earthlink.net>
2017-01-29 23:44:12 -0500

    liri add

---
+++ liri.js
@@ -0,0 +1,36 @@
+// top
+
+
+//variables
+var debug = true;  //console control
+
+var twitter = "my-tweets";
+var spotify = "spotify-this-song";
+var omdb = "movie-this";
+var request = "do-what-it-says";
+
+
+
+//console log
+if (debug) {
+  console.log(twitter);
+  console.log(spotify);
+  console.log(omdb);
+  console.log(request);
+}
+
+
+
+
+
+
+//commands
+
+
+// my-tweets
+
+// spotify-this-song
+
+// movie-this
+
+// do-what-it-says
