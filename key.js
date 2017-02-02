ab99a72eacd386e1b468badc27e3042a5aa2f1da
rob bee <rbenny@earthlink.net>
2017-02-02 16:58:08 -0500

    empty key

---
+++ key.js
@@ -0,0 +1,16 @@
+console.log('this is loaded');
+
+// const Twitter = require('./node_modules/twitter')
+
+exports.twitterKeys = {
+  consumer_key: 'xxx',
+  consumer_secret: 'xxx',
+  access_token_key: 'xxx',
+  access_token_secret: 'xxx',
+}
+
+
+// const params = {screen_name: 'robbeeio'};
+// client.get('statuses/user_timeline', params)
+//   .then(console.log)
+//   .catch(console.error)
